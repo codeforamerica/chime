1a074f5689de157aa1ffa11062e49251e8045666
Michal Migurski <mike@teczno.com>
2014-07-01 22:12:43 -0700

    Added SINGLE_USER flag

--- bizarro/__init__.py
+++ bizarro/__init__.py
@@ -8,6 +8,7 @@ app.secret_key = 'boop'
 app.config['WORK_PATH'] = environ.get('WORK_PATH', '.')
 app.config['REPO_PATH'] = environ.get('REPO_PATH', 'sample-site')
 app.config['BROWSERID_URL'] = environ.get('BROWSERID_URL', 'http://127.0.0.1:5000')
+app.config['SINGLE_USER'] = bool(environ.get('SINGLE_USER', False))
 app.config['default_branch'] = 'master'
 
 from . import views
