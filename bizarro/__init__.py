fb153478cdbf223aa355e72be16dc00f19626646
Michal Migurski <mike@teczno.com>
2014-07-06 20:14:37 -0700

    Using WORK_PATH to place working repositories

--- bizarro/__init__.py
+++ bizarro/__init__.py
@@ -5,7 +5,7 @@ from . import repo_functions, edit_functions, jekyll_functions
 
 app = Flask(__name__)
 app.secret_key = 'boop'
-app.config['WORK_PATH'] = '.'
+app.config['WORK_PATH'] = environ.get('WORK_PATH', '.')
 app.config['REPO_PATH'] = environ.get('REPO_PATH', 'sample-site')
 app.config['default_branch'] = 'master'
 
