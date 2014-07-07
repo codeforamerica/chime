ed24b951cb24f14de3cb132211e9f2b54cb5ca42
Michal Migurski <migurski@bizarro-chef.(none)>
2014-07-07 03:40:20 +0000

    Added bizarro user

--- bizarro/recipes/default.rb
+++ bizarro/recipes/default.rb
@@ -10,28 +10,33 @@ execute "pip install -r requirements.txt" do
   cwd "/opt/bizarro-cms"
 end
 
+group node[:user]
+user node[:user] do
+  gid node[:user]
+end
+
 directory "/var/opt/bizarro-work" do
-  owner "migurski"
-  group "migurski"
+  owner node[:user]
+  group node[:user]
   mode "0775"
 end
 
 directory "/var/opt/sample-site" do
-  owner "migurski"
-  group "migurski"
+  owner node[:user]
+  group node[:user]
   mode "0775"
 end
 
 bash "tar -xzf sample-site.tar.gz" do
   code "tar -C /var/opt -xzf /opt/bizarro-cms/sample-site.tar.gz"
-  user "migurski"
+  user node[:user]
   creates "/var/opt/sample-site/config"
 end
 
 env_file = File.realpath(File.join(File.dirname(__FILE__), 'honcho-env'))
 
 execute "honcho export upstart /etc/init" do
-  command "honcho -e #{env_file} export -u migurski -a bizarro-cms upstart /etc/init"
+  command "honcho -e #{env_file} export -u #{node[:user]} -a bizarro-cms upstart /etc/init"
   cwd "/opt/bizarro-cms"
 end
 
