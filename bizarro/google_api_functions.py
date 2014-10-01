0db1ab81931c01ec570f5769b67b2f8cf20fe8c5
Erica Kwan <erica@codeforamerica.org>
2014-10-01 13:59:05 -0700

    In fact, no need to escape at all

--- bizarro/google_api_functions.py
+++ bizarro/google_api_functions.py
@@ -47,10 +47,9 @@ def fetch_google_analytics_for_page(page_path, access_token):
     start_date = (date.today() - timedelta(days=7)).isoformat()
     end_date = date.today().isoformat()
     profile_id = os.environ.get('PROFILE_ID')
-    escaped_page_path = page_path.replace('+', '\+')
     query_string = urlencode({'ids' : 'ga:' + profile_id, 'dimensions' : 'ga:previousPagePath,ga:pagePath',
                                'metrics' : 'ga:pageViews,ga:avgTimeOnPage,ga:exitRate',
-                               'filters' : 'ga:pagePath=~/' + escaped_page_path, 'start-date' : start_date,
+                               'filters' : 'ga:pagePath=~/' + page_path, 'start-date' : start_date,
                                'end-date' : end_date, 'max-results' : '1', 'access_token' : access_token})
     resp = get('https://www.googleapis.com/analytics/v3/data/ga' + '?' + query_string)
     return json.loads(resp.content)
