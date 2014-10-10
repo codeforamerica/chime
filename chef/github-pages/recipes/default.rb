f235db4491ec9bcc49db99f666d7039917450a4b
Michal Migurski <mike@teczno.com>
2014-10-09 18:14:27 -0700

    Added Chef recipe for Jekyll

---
+++ chef/github-pages/recipes/default.rb
@@ -0,0 +1,10 @@
+package 'ruby1.9.3'
+
+gem_package 'github-pages' do
+    gem_binary '/usr/bin/gem1.9.3'
+    options "--no-rdoc --no-ri"
+    action :install
+end
+
+# Jekyll wants a JS runtime
+package 'nodejs'
