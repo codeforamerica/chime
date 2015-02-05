5b7b445ea84f775eb7f96281e850cf8160003893
Michal Migurski <mike@codeforamerica.org>
2015-02-04 23:00:35 -0800

    Moved repository creation to dedicated chef recipe

--- chef/bizarro/recipes/default.rb
+++ chef/bizarro/recipes/default.rb
@@ -1,6 +1,5 @@
 include_recipe "python"
-include_recipe "account"
-package "git"
+include_recipe "repository"
 
 #
 # Put code where it needs to be.
@@ -23,71 +22,6 @@ directory "/var/opt/bizarro-work" do
   owner name
   group name
   mode "0775"
-end
-
-python "create Github repository" do
-  user name
-  code <<-GITHUB
-import json, requests
-
-info = dict(
-    name='test-repo',
-    private=False, has_issues=False, has_wiki=False, has_downloads=False
-    )
-
-url = 'https://api.github.com/orgs/ceviche/repos'
-head = {'Content-Type': 'application/json'}
-auth = ('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', 'x-oauth-basic')
-resp = requests.post(url, json.dumps(info), headers=head, auth=auth)
-code = resp.status_code
-
-if code == 422:
-    pass # Github repository already exists
-elif code not in range(200, 299):
-    raise RuntimeError('Github repository creation failed, status {}'.format(code))
-
-with open('/home/#{name}/.ssh/id_rsa.pub') as file:
-    input = dict(title='test-key', key=file.read())
-
-url = 'https://api.github.com/repos/ceviche/test-repo/keys'
-resp = requests.post(url, json.dumps(input), headers=head, auth=auth)
-code = resp.status_code
-
-if code == 422:
-    pass # Github deploy key already exists
-elif code not in range(200, 299):
-    raise RuntimeError('Github deploy key creation failed, status {}'.format(code))
-
-GITHUB
-end
-
-bash "init bare /var/opt/bizarro-site" do
-  user 'root'
-  flags '-e'
-  code <<-GIT
-DIR=`mktemp -d /tmp/bizarro-site-XXXXXX`
-git init --shared=group --bare $DIR
-
-chown -R #{name}:#{name} $DIR
-chmod -R ug+rwX $DIR
-chmod -R o+rX $DIR
-
-rm -rf /var/opt/bizarro-site
-mv $DIR /var/opt/bizarro-site
-GIT
-end
-
-bash "clone from codeforamerica/ceviche-starter to Github origin remote" do
-  user name
-  flags '-e'
-  cwd '/var/opt/bizarro-site'
-  code <<-GIT
-git fetch https://github.com/codeforamerica/ceviche-starter.git
-git branch master FETCH_HEAD
-
-git remote add origin git@github.com:ceviche/test-repo.git
-git push -u origin master
-GIT
 end
 
 #
---
+++ chef/repository/recipes/default.rb
@@ -0,0 +1,78 @@
+include_recipe "account"
+package "git"
+
+name = node[:user]
+starter_repo = node[:starter]
+github_repo = ENV['GITHUB_REPO']
+github_token = ENV['GITHUB_TOKEN']
+
+directory "/var/opt/bizarro-work" do
+  owner name
+  group name
+  mode "0775"
+end
+
+python "create Github repository" do
+  user name
+  code <<-GITHUB
+import json, requests
+
+info = dict(
+    name='#{github_repo}',
+    private=False, has_issues=False, has_wiki=False, has_downloads=False
+    )
+
+url = 'https://api.github.com/orgs/ceviche/repos'
+head = {'Content-Type': 'application/json'}
+auth = ('#{github_token}', 'x-oauth-basic')
+resp = requests.post(url, json.dumps(info), headers=head, auth=auth)
+code = resp.status_code
+
+if code == 422:
+    pass # Github repository already exists
+elif code not in range(200, 299):
+    raise RuntimeError('Github repository creation failed, status {}'.format(code))
+
+with open('/home/#{name}/.ssh/id_rsa.pub') as file:
+    input = dict(title='test-key', key=file.read())
+
+url = 'https://api.github.com/repos/ceviche/#{github_repo}/keys'
+resp = requests.post(url, json.dumps(input), headers=head, auth=auth)
+code = resp.status_code
+
+if code == 422:
+    pass # Github deploy key already exists
+elif code not in range(200, 299):
+    raise RuntimeError('Github deploy key creation failed, status {}'.format(code))
+
+GITHUB
+end
+
+bash "init bare /var/opt/bizarro-site" do
+  user 'root'
+  flags '-e'
+  code <<-GIT
+DIR=`mktemp -d /tmp/bizarro-site-XXXXXX`
+git init --shared=group --bare $DIR
+
+chown -R #{name}:#{name} $DIR
+chmod -R ug+rwX $DIR
+chmod -R o+rX $DIR
+
+rm -rf /var/opt/bizarro-site
+mv $DIR /var/opt/bizarro-site
+GIT
+end
+
+bash "clone from starter repo to Github origin remote" do
+  user name
+  flags '-e'
+  cwd '/var/opt/bizarro-site'
+  code <<-GIT
+git fetch #{starter_repo}
+git branch master FETCH_HEAD
+
+git remote add origin git@github.com:ceviche/#{github_repo}.git
+git push -u origin master
+GIT
+end
