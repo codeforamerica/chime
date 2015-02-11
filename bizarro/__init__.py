5dc8b85093d13f078b893039703f7dc34d77b321
Michal Migurski <mike@teczno.com>
2015-02-11 15:24:13 -0800

    Setting up main logger with correct debug level initially

--- bizarro/__init__.py
+++ bizarro/__init__.py
@@ -1,5 +1,6 @@
 from os import environ
-from flask import Flask
+from flask import Flask, current_app
+from logging import getLogger, DEBUG
 
 from . import repo_functions, edit_functions, jekyll_functions
 
@@ -10,5 +11,12 @@ app.config['REPO_PATH'] = environ.get('REPO_PATH', 'sample-site')
 app.config['BROWSERID_URL'] = environ.get('BROWSERID_URL', 'http://127.0.0.1:5000')
 app.config['SINGLE_USER'] = bool(environ.get('SINGLE_USER', False))
 app.config['default_branch'] = 'master'
+
+@app.before_first_request
+def before_first_request():
+    '''
+    '''
+    if current_app.debug:
+        getLogger('bizarro').setLevel(DEBUG)
 
 from . import views
