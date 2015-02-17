76aa0f0c80a768ebfffdd6c0d42a70a4ae234a42
Thomas Apodaca <tma@angrymonkey.com>
2015-02-17 12:53:33 -0800

    got running with application factory

--- bizarro/__init__.py
+++ bizarro/__init__.py
@@ -4,19 +4,25 @@ from logging import getLogger, DEBUG
 
 from . import repo_functions, edit_functions, jekyll_functions
 
-app = Flask(__name__)
-app.secret_key = 'boop'
-app.config['WORK_PATH'] = environ.get('WORK_PATH', '.')
-app.config['REPO_PATH'] = environ.get('REPO_PATH', 'sample-site')
-app.config['BROWSERID_URL'] = environ.get('BROWSERID_URL', 'http://127.0.0.1:5000')
-app.config['SINGLE_USER'] = bool(environ.get('SINGLE_USER', False))
-app.config['default_branch'] = 'master'
+def create_app():
+	app = Flask(__name__)
+	app.secret_key = 'boop'
+	app.config['WORK_PATH'] = environ.get('WORK_PATH', '.')
+	app.config['REPO_PATH'] = environ.get('REPO_PATH', 'sample-site')
+	app.config['BROWSERID_URL'] = environ.get('BROWSERID_URL', 'http://127.0.0.1:5000')
+	app.config['SINGLE_USER'] = bool(environ.get('SINGLE_USER', False))
+	app.config['default_branch'] = 'master'
 
-@app.before_first_request
-def before_first_request():
-    '''
-    '''
-    if current_app.debug:
-        getLogger('bizarro').setLevel(DEBUG)
+	# attach routes and custom error pages here
 
-from . import views
+	return app
+
+# @app.before_first_request
+# def before_first_request():
+#     '''
+#     '''
+#     if current_app.debug:
+#         getLogger('bizarro').setLevel(DEBUG)
+
+# from . import views
+
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from bizarro import app
-
+from bizarro import create_app
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True)
