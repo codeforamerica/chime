c2afc1b43a874be2ad9f87da4461476737105b56
Thomas Apodaca <tma@angrymonkey.com>
2015-02-17 13:17:06 -0800

    changed tabs to spaces

--- bizarro/__init__.py
+++ bizarro/__init__.py
@@ -5,17 +5,17 @@ from logging import getLogger, DEBUG
 from . import repo_functions, edit_functions, jekyll_functions
 
 def create_app():
-	app = Flask(__name__)
-	app.secret_key = 'boop'
-	app.config['WORK_PATH'] = environ.get('WORK_PATH', '.')
-	app.config['REPO_PATH'] = environ.get('REPO_PATH', 'sample-site')
-	app.config['BROWSERID_URL'] = environ.get('BROWSERID_URL', 'http://127.0.0.1:5000')
-	app.config['SINGLE_USER'] = bool(environ.get('SINGLE_USER', False))
-	app.config['default_branch'] = 'master'
+    app = Flask(__name__)
+    app.secret_key = 'boop'
+    app.config['WORK_PATH'] = environ.get('WORK_PATH', '.')
+    app.config['REPO_PATH'] = environ.get('REPO_PATH', 'sample-site')
+    app.config['BROWSERID_URL'] = environ.get('BROWSERID_URL', 'http://127.0.0.1:5000')
+    app.config['SINGLE_USER'] = bool(environ.get('SINGLE_USER', False))
+    app.config['default_branch'] = 'master'
 
-	# attach routes and custom error pages here
+    # attach routes and custom error pages here
 
-	return app
+    return app
 
 # @app.before_first_request
 # def before_first_request():
@@ -24,5 +24,5 @@ def create_app():
 #     if current_app.debug:
 #         getLogger('bizarro').setLevel(DEBUG)
 
-# from . import views
+from . import views
 
