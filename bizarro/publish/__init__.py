9f1f02fe888eb180de7452a47a80881e8ab1246f
Michal Migurski <mike@teczno.com>
2015-03-06 14:37:46 -0800

    Stubbed in publish app for receiving webhooks

---
+++ bizarro/publish/__init__.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, Flask
+from logging import getLogger, DEBUG
+
+publish = Blueprint('bizarro.publish', __name__, template_folder='templates')
+
+def create_app(environ):
+    app = Flask(__name__, static_folder='static')
+    app.secret_key = 'boop'
+
+    # attach routes and custom error pages here
+    app.register_blueprint(publish)
+
+    @app.before_first_request
+    def before_first_request():
+        '''
+        '''
+        if app.debug:
+            getLogger('bizarro.publish').setLevel(DEBUG)
+
+    return app
+
+@publish.route('/')
+def index():
+    return 'Hello world'
