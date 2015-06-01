0a1e04ca17db4f33e592d9d1aeb52ce12c5d6714
William Pietri <william@scissor.com>
2015-06-01 11:03:44 -0700

    Fixing deployment issue created by previous change.

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
 from setuptools import setup
-from os.path import join, dirname
-import sys
-
-#with open(join(dirname(__file__), 'openaddr', 'VERSION')) as file:
-#    version = file.read().strip()
 
 setup(
     name = 'Chime',
@@ -11,7 +6,7 @@ setup(
     url = 'https://github.com/chimecms/chime',
     author = 'Code for America',
     description = 'Hashing out some ideas about Git, Jekyll, language, and content management',
-    packages = ['chime', 'chime.httpd', 'chime.publish', 'chime.setup'],
+    packages = ['chime', 'chime.httpd', 'chime.publish', 'chime.instantiation'],
     package_data = {
         'chime': []
     },
