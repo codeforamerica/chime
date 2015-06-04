7c5ff448f1eb6125f3403505c453a612a5b9b73a
William Pietri <william@scissor.com>
2015-06-03 17:57:59 -0700

    Replaced nose_parameterized with something that produces more useful names.

--- test/acceptance/test_preview.py
+++ test/acceptance/test_preview.py
@@ -2,7 +2,7 @@
 
 import os
 from unittest import main, TestCase
-from nose_parameterized import parameterized, param
+import re
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -12,24 +12,26 @@ import sys
 
 
 def browser_version(list):
-    return map(lambda v:{"browser_version": v}, list)
+    return map(lambda v: {"browser_version": v}, list)
+
 
 def add(key, value, list_of_dicts):
     map(lambda d: d.update({key: value}), list_of_dicts)
 
     return list_of_dicts
 
+
 def with_os(os_name, list_of_dicts):
     return add('os', os_name, list_of_dicts)
+
 
 def browser(browser_name, list_of_dicts):
     return add('browser', browser_name, list_of_dicts)
 
+
 def os_version(version, list_of_dicts):
     return add('os_version', version, list_of_dicts)
 
-c1 = {'browser': 'Firefox', 'browser_version': '35.0', 'os': 'Windows', 'os_version': '8.1',
-          'resolution': '1024x768'}
 
 def latest_firefox_and_chrome():
     browsers = []
@@ -46,17 +48,16 @@ def windows_8_targets():
     return result
 
 
-
 def windows_targets():
     # TODO: find out actual targets
     result = []
     result.extend(windows_8_targets())
     return result
 
+
 def macos_targets():
     # TODO: find out actual targets
     return []
-
 
 
 def mobile_targets():
@@ -71,20 +72,30 @@ def all_targets():
     result.extend(mobile_targets())
     return add('project', 'chime', result)
 
-def all_targets_for_expansion():
-    return map(lambda t: param(t), all_targets())
 
-targets = all_targets_for_expansion()
-class TestSelenium(TestCase):
+BROWSERS_TO_TRY = all_targets()
+
+
+class TestPreview(TestCase):
     def setUp(self):
         self.email = os.environ['TESTING_EMAIL']
         self.password = os.environ['TESTING_PASSWORD']
         self.live_site = 'http://' + self.load_hosts_file()[0]
 
-    def use_driver(self, capabilities):
-        self.driver = webdriver.Remote(
-            command_executor='http://chimetests1:AxRS34k3vrf7mu9zZ2hE@hub.browserstack.com:80/wd/hub',
-            desired_capabilities=capabilities)
+    def use_driver(self, capabilities=None):
+        """
+        :param capabilities: If particular capabilities are requested; they are passed along to BrowserStack.
+            Otherwise, a local Firefox is used.
+        """
+        if capabilities:
+            sys.stderr.write("Setting up for browser %s\n" % capabilities)
+            self.driver = webdriver.Remote(
+                command_executor='http://chimetests1:AxRS34k3vrf7mu9zZ2hE@hub.browserstack.com:80/wd/hub',
+                desired_capabilities=capabilities)
+
+        else:
+            self.driver = webdriver.Firefox()
+            sys.stderr.write("Set up for Firefox\n")
         self.waiter = WebDriverWait(self.driver, timeout=60)
 
     def load_hosts_file(self):
@@ -109,12 +120,8 @@ class TestSelenium(TestCase):
 
         return predicate
 
-
-    @parameterized.expand(targets)
-    def test_create_page_and_preview(self, browsercap):
-        test_on = browsercap
-        sys.stderr.write("Now testing on %s\n" % browsercap)
-        self.use_driver(test_on)
+    def test_create_page_and_preview(self, browsercap=None):
+        self.use_driver(browsercap)
 
         self.driver.get(self.live_site)
         main_window = self.driver.current_window_handle
@@ -178,6 +185,34 @@ class TestSelenium(TestCase):
 
     def tearDown(self):
         self.driver.quit()
+
+
+def safe_name(string):
+    return re.sub('\W', '', re.sub('[.]', '_', string.lower()))
+
+
+def safe_browser_name(d):
+    return '_'.join([safe_name(d[n]) for n in ['os', 'os_version', 'browser', 'browser_version']])
+
+
+def rewrite_for_all_browsers(try_these):
+    """
+        Magically make test methods for all desired browsers. Note that this method cannot contain
+        the word 'test' or nose will decide it is worth running.
+    """
+    # TODO: find test methods and rewrite them all
+    name = 'test_create_page_and_preview'
+    m = getattr(TestPreview, name)
+    for browser in try_these:
+        new_name = "{name}_{browser}".format(name=name, browser=safe_browser_name(browser))
+        new_function = lambda i, b=browser: m(i, b)
+        new_function.__name__ = new_name
+        setattr(TestPreview, new_name, new_function)
+    delattr(TestPreview, name)
+
+
+if BROWSERS_TO_TRY:
+    rewrite_for_all_browsers(BROWSERS_TO_TRY)
 
 if __name__ == '__main__':
     main()
