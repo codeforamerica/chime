9e7ebfa79b9599d3518e79da405f765a8d676056
Norris Hung <norrishung@Norriss-MacBook-Pro.local>
2015-06-17 15:55:06 -0700

    working on markdown formatting bar

--- chime/static/javascript/markdown-bar.js
+++ chime/static/javascript/markdown-bar.js
@@ -9,14 +9,66 @@ function MarkdownBar(bar, textarea) {
 			'name': 'h1',
 			'syntax': '# ${content}',
 			'icon': 'h1',
-			'filler': 'This is a level 1 heading'
+			'filler': 'This is a level 1 heading',
+			'type': 'block'
 		},
 		{
 			'name': 'h2',
 			'syntax': '## ${content}',
 			'icon': 'h2',
-			'filler': 'This is a level 2 heading'
-		}
+			'filler': 'This is a level 2 heading',
+			'type': 'block'
+		},
+		{
+			'name': 'h3',
+			'syntax': '### ${content}',
+			'icon': 'h3',
+			'filler': 'This is a level 3 heading',
+			'type': 'block'
+		},
+		{
+			'name': 'italics',
+			'syntax': '_${content}_',
+			'icon': '<i>i</i>',
+			'filler': 'This is italicized text',
+			'type': 'inline'
+		},
+		{
+			'name': 'bold',
+			'syntax': '**${content}**',
+			'icon': '<b>b</b>',
+			'filler': "This is bold text",
+			'type': 'inline'
+		},
+		{
+			'name': 'link',
+			'syntax': '[${content}](http://www.example.com)',
+			'icon': 'link',
+			'filler': "This is a link",
+			'type': 'inline'
+		},
+		{
+			'name': 'unordered-list',
+			'syntax': '- ${content}',
+			'icon': 'ul',
+			'filler': "This is an bulleted list item",
+			'type': 'block'
+		},
+		{
+			'name': 'ordered-list',
+			'syntax': '1. ${content}',
+			'icon': 'ol',
+			'filler': "This is an numbered list item",
+			'type': 'block'
+		},
+		{
+			'name': 'blockquote',
+			'syntax': '> ${content}',
+			'icon': 'blockquote',
+			'filler': "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id blanditiis voluptatem odit nesciunt. Fugit ipsam saepe, quisquam iste mollitia ducimus, recusandae, voluptatum libero eligendi nam sequi hic. Libero, tempore, suscipit!",
+			'type': 'block'
+		},
+
 	];
 
 	// String interpolation function
@@ -49,36 +101,92 @@ function MarkdownBar(bar, textarea) {
 	};
 
 
-	var markdownify = function(event, syntax, filler) {
+	var markdownify = function(event, syntax, filler, patternType) {
 		event.preventDefault();
 
-		// Figure out whether function should:
-		//	a. add markdown pattern to current cursor position
-		//  b. append text to top of page
-		//	c. replace selection with markdowned version of text
+		// Inline Pattern Behavior
 
+		// Single Empty line: insert pattern with filler text with filler text selected, cursor at selection beginning
+		// Single Non-empty line: insert pattern with filler text with filler text selected, cursor at selection beginning
+		// Single Line && Selection: replace selection with filler text, cursor at selection end.
+
+		// Multiple Lines: Break up into single lines and do same as above for each line.
+
+
+		// Block Pattern Behavior
+
+		// Single Empty line: Create pattern (with appropriate whitespace) with filler text selected, cursor at selection beginning
+		// Single Non-Empty line: Make entire line block pattern with cursor at end
+		// Single Line && Selection: Make entire line block pattern with cursor at end
+
+		// Multiple Empty Lines: Create pattern (with appropriate whitespace) with filler text selected, cursor at selection beginning.
+		// Multiple Non-Empty Lines: Break up to individual lines and treat each as a single non-empty line
+		// Multiple Lines with Partial or Complete Selection: Break up into individual lines and treat as single non-empty lines
+
+		// ** For mixed multiline selections, keep empty lines as empty lines.
+
+		//TODO: Fix selectionStart and selectionEnd to work with IE
 		var selectionStart = markdownTextarea.get(0).selectionStart;
 		var selectionEnd =  markdownTextarea.get(0).selectionEnd;
+		
+		var isSelection = selectionStart == selectionEnd ? false : true;
 
-		var textSelected = selectionStart == selectionEnd ? false : true;
+		// If patterntype is block, content should begin at beginning of line and end at the end of line
+		if(patternType == "block") {
+			while(markdownTextarea.val().charAt(selectionStart-1) != '\n' && selectionStart > 0) {
+				selectionStart--;
+			}
+			while(markdownTextarea.val().charAt(selectionEnd) != '\n' && selectionEnd < markdownTextarea.val().length) {
+				selectionEnd++;
+			}
+		}
 
-		var content = textSelected ? markdownTextarea.val().slice(selectionStart, selectionEnd) : filler;
-		var newContent = interpolate(syntax, {content: content});
+		// Iterate over each line.
+		// TODO: Deal with swapping of block patterns (turning a list item into a header)
+		var newContent = "";
+		var content = markdownTextarea.val().slice(selectionStart, selectionEnd).split(/\n/);
+		$(content).each(function(index, contentLine) {
 
-		markdownTextarea.val(markdownTextarea.val().slice(0, selectionStart) + newContent + markdownTextarea.val().slice(selectionEnd, markdownTextarea.val().length))
+			if(contentLine == "") {
+				contentLine = filler;
+			}
+			
+			newContent = newContent + interpolate(syntax, {content: contentLine});
+			if(index < content.length-1) {
+				newContent = newContent + '\n';
+			}
+		});
 
+
+		// Update undo/redo queue is possible with execCommand. Otherwise, replace text in textarea.
+		// TODO: Would mutationObservers be a better way to implement this?
+		if (document.queryCommandSupported('insertText')) {
+			markdownTextarea.focus();
+			//TODO: Fix selectionStart and selectionEnd to work with IE
+			markdownTextarea.get(0).selectionStart = selectionStart;
+			markdownTextarea.get(0).selectionEnd = selectionEnd;
+		    document.execCommand('insertText', false, newContent);
+		}
+		else {
+			var contentBefore = markdownTextarea.val().slice(0, selectionStart);
+			var contentAfter = markdownTextarea.val().slice(selectionEnd, markdownTextarea.val().length)
+			markdownTextarea.val(contentBefore + newContent + contentAfter)
+		}
 	}
 
 	
 	this.init = function() {
+
 		// Add buttons to markdown bar and bind events
 		$(PATTERNS).each(function(index, pattern) {
 			var patternButton = $('<div class="button button--outline toolbar__item">' + pattern['icon'] +'</div>');
 			patternButton.bind('click', function(event) {
-				markdownify(event, pattern['syntax'], pattern['filler']);
+				markdownify(event, pattern['syntax'], pattern['filler'], pattern['type']);
 			});
 			markdownBar.append(patternButton);
 		});
+
+		// TODO: Add undo/redo buttons
 	}
 
 	this.init();
