872657e6dadef220e34894f881ec3d79b415f329
William Pietri <william@scissor.com>
2015-07-06 14:13:11 -0700

    Acceptance tests that works on core browsers except IE11 on Win 8.1

--- test/acceptance/test_preview.py
+++ test/acceptance/test_preview.py
@@ -1,104 +1,66 @@
 # -- coding: utf-8 --
 
 import os
-from random import random, randint
-from unittest import main, TestCase
-import re
+from random import randint
+from unittest import main
+import sys
+import time
+import urllib
+
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import sys
+from acceptance.browser import Browser
+
+from acceptance.chime_test_case import ChimeTestCase
 
 
-def browser_version(list):
-    return map(lambda v: {"browser_version": v}, list)
+BROWSERS_TO_TRY = Browser.from_string(os.environ.get('BROWSERS_TO_TEST'))
+TEST_REPETITIONS = int(os.environ.get('TEST_REPETITIONS',"1"))
 
 
-def add(key, value, list_of_dicts):
-    map(lambda d: d.update({key: value}), list_of_dicts)
-
-    return list_of_dicts
-
-
-def with_os(os_name, list_of_dicts):
-    return add('os', os_name, list_of_dicts)
-
-
-def browser(browser_name, list_of_dicts):
-    return add('browser', browser_name, list_of_dicts)
-
-
-def os_version(version, list_of_dicts):
-    return add('os_version', version, list_of_dicts)
-
-
-def latest_firefox_and_chrome():
-    browsers = []
-    browsers.extend(browser('Firefox', browser_version(['35.0'])))
-    browsers.extend(browser('Chrome', browser_version(['39.0'])))
-    return browsers
-
-
-def windows_8_targets():
-    result = []
-    browsers = browser('IE', browser_version(["11.0"]))
-    browsers.extend(latest_firefox_and_chrome())
-    result.extend(with_os('Windows', os_version('8.1', browsers)))
-    return result
-
-
-def windows_targets():
-    # TODO: find out actual targets
-    result = []
-    result.extend(windows_8_targets())
-    return result
-
-
-def macos_targets():
-    # TODO: find out actual targets
-    return []
-
-
-def mobile_targets():
-    # TODO: find out actual targets
-    return []
-
-
-def all_targets():
-    result = []
-    result.extend(macos_targets())
-    result.extend(windows_targets())
-    result.extend(mobile_targets())
-    return add('project', 'chime', result)
-
-
-BROWSERS_TO_TRY = all_targets()
-
-
-class TestPreview(TestCase):
+class TestPreview(ChimeTestCase):
     _multiprocess_can_split_ = True
     def setUp(self):
-        self.email = os.environ['TESTING_EMAIL']
+        self.email = self.mutate_email(os.environ['TESTING_EMAIL'])
         self.password = os.environ['TESTING_PASSWORD']
-        self.live_site = 'http://' + self.load_hosts_file()[0]
+        self.host = self.load_hosts_file()[0]
+        self.live_site = 'http://' + self.host
 
-    def use_driver(self, capabilities=None):
+    def mutate_email(self, email):
+        return email.replace('@', '+' + self.random_digits() + '@')
+
+    def use_driver(self, browser=None):
         """
-        :param capabilities: If particular capabilities are requested; they are passed along to BrowserStack.
+        :param capabilities: If particular browser are requested; it is passed along to BrowserStack.
             Otherwise, a local Firefox is used.
         """
-        if capabilities:
-            sys.stderr.write("Setting up for browser %s\n" % capabilities)
-            self.driver = webdriver.Remote(
-                command_executor='http://chimetests1:AxRS34k3vrf7mu9zZ2hE@hub.browserstack.com:80/wd/hub',
-                desired_capabilities=capabilities)
+        if browser:
+            sys.stderr.write("Setting up for browser %s\n" % browser)
+            mode = 'browserstack'
+            if mode=='browserstack':
+                capabilities = browser.as_browserstack_capabilities({'browserstack.debug': True, 'project': 'chime'})
+
+                if browser.browser == 'IE':
+                    capabilities['ignoreProtectedModeSettings'] = True
+                self.driver = webdriver.Remote(
+                    command_executor='http://chimetests1:AxRS34k3vrf7mu9zZ2hE@hub.browserstack.com:80/wd/hub',
+                    desired_capabilities=capabilities)
+            elif mode=='saucelabs':
+                self.driver = webdriver.Remote(
+                    command_executor="http://wpietri:42e45709-70f3-4eb8-9ebd-c85be6dfec7a@ondemand.saucelabs.com:80/wd/hub",
+                    desired_capabilities=browser.as_saucelabs_capabilities())
+            else:
+                raise ValueError
 
         else:
-            self.driver = webdriver.Firefox()
-            sys.stderr.write("Set up for Firefox\n")
+            self.driver = webdriver.Chrome(executable_path='/usr/lib/chromium-browser/chromedriver')
+            sys.stderr.write("Set up for Chrome\n")
         self.waiter = WebDriverWait(self.driver, timeout=60)
+        self.driver.implicitly_wait(15) # seconds
 
     def load_hosts_file(self):
         with open(os.path.dirname(__file__) + '/../../fabfile/hosts.txt', 'rU') as f:
@@ -122,32 +84,54 @@ class TestPreview(TestCase):
 
         return predicate
 
-    def test_create_page_and_preview(self, browsercap=None):
-        self.use_driver(browsercap)
 
-        self.driver.get(self.live_site)
-        main_window = self.driver.current_window_handle
-        self.log_in_to_persona(main_window)
 
-        # make sure we're back to Chime
-        # self.assertEquals(self.driver.title, 'Tasks | Chime')
+    def test_create_page_and_preview(self, browser=None):
+        self.use_driver(browser)
+
+        signin_method = "stubby_js"
+        if signin_method=='magic_url':
+            signin_link = self.live_site + '/sign-in?assertion={}\n'.format(urllib.quote_plus(self.email))
+            sys.stderr.write("connecting to {}".format(signin_link))
+            self.driver.get(signin_link)
+            time.sleep(1)
+        elif signin_method=='stubby_js':
+           self.driver.get(self.live_site)
+           self.driver.execute_script('window.navigator.id.stubby.setPersonaState("{}")'.format(self.email))
+        elif signin_method=='stubby_ui':
+            self.driver.get(self.live_site)
+            main_window = self.driver.current_window_handle
+            self.driver.find_element_by_id('signin').click()
+            alert = self.driver.switch_to.alert
+            alert.send_keys(self.email)
+            alert.accept()
+            self.driver.switch_to.window(main_window)
+        else:
+            raise ValueError
+
+        self.driver.get(self.live_site) # this line works around an IE8 issue where quirks mode is sometimes triggered
+
+        # make sure we have logged in
+        # self.assertEqual("Current Activities", self.driver.find_element_by_css_selector('h2.current-tasks__title').text)
 
         main_url = self.driver.current_url
 
+        sys.stderr.write("at url {}\n".format(main_url))
+
         # start a new task
         task_description = self.marked_string('task_description')
-        self.waiter.until(EC.visibility_of_element_located((By.NAME, 'task_description'))).send_keys(
-            task_description)
+        self.driver.find_element_by_name('task_description').send_keys(task_description)
         self.driver.find_element_by_name('task_beneficiary').send_keys(self.marked_string('task_beneficiary'))
+        main_window = self.driver.current_window_handle
         self.driver.find_element_by_class_name('create').click()
 
         # create a new page
-        self.waiter.until(
-            EC.visibility_of_element_located((By.XPATH, "//form[@action='.']/input[@name='path']"))).send_keys(self.marked_string('filename'))
+        self.driver.find_element_by_xpath("//form[@action='.']/input[@name='path']").send_keys(self.marked_string('filename'))
         self.driver.find_element_by_xpath("//form[@action='.']/input[@type='submit']").click()
 
+
         # add some content to that page
-        self.waiter.until(EC.visibility_of_element_located((By.NAME, 'en-title'))).send_keys(self.marked_string('title'))
+        self.driver.find_element_by_name('en-title').send_keys(self.marked_string('title'))
         test_page_content = 'This is some test content.', Keys.RETURN, Keys.RETURN, self.marked_string('body_text'),\
                             Keys.RETURN, Keys.RETURN, '# This is a test h1', Keys.RETURN, Keys.RETURN, '> This is a test pull-quote'
         self.driver.find_element_by_id('en-body markItUp').send_keys(*test_page_content)
@@ -156,23 +140,31 @@ class TestPreview(TestCase):
         self.driver.find_element_by_xpath("//input[@value='Save']").click()
 
         # preview the page
-        self.waiter.until(EC.presence_of_element_located((By.XPATH, "//a[@target='_blank']"))).click()
+        self.driver.find_element(By.XPATH, "//a[@target='_blank']").click()
 
         # wait until the preview window's available and switch to it
         self.waiter.until(self.switch_to_other_window(main_window))
-
         self.waiter.until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
         self.driver.close()
         self.driver.switch_to.window(main_window)
+        # TODO: make sure there's something there
 
         # go back to the main page
         self.driver.get(main_url)
         # delete our branch
-        self.driver.find_element_by_xpath("//a[contains(text(),'{}')]/../..//button[@value='abandon']".format(task_description)).click()
+        delete_button = self.driver.find_element_by_xpath(
+            "//a[contains(text(),'{}')]/../..//button[@value='abandon']".format(task_description))
+        self.scrollTo(delete_button)
+        delete_button.click()
         # TODO: make sure deletion happens
 
+    def random_digits(self, count=6):
+        format_as = "{:0" + str(count) + "d}"
+        return format_as.format(randint(0, 10 ** count - 1))
+
     def marked_string(self, base='string'):
-        return "{}-{:06d}".format(base,randint(0,999999))
+        return "{}-{}".format(base, self.random_digits())
+
 
     def log_in_to_persona(self, main_window):
         self.driver.find_element_by_id('signin').click()
@@ -189,36 +181,51 @@ class TestPreview(TestCase):
         # switch back to the main window
         self.driver.switch_to.window(main_window)
 
+    def onFailure(self, exception_info):
+        super(TestPreview, self).onFailure(exception_info)
+        self.screenshot()
+
+    def screenshot(self):
+        if self.driver:
+            self.driver.save_screenshot("at-{}.png".format(time.strftime('%Y%m%d-%H%M%S', time.localtime())))
+        else:
+            sys.stderr.write("no driver to take screenshot\n")
+
+    def onError(self, exception_info):
+        super(TestPreview, self).onError(exception_info)
+        self.screenshot()
+
     def tearDown(self):
-        self.driver.quit()
+        if self.driver:
+            self.driver.quit()
+        pass
+
+    def scrollTo(self, element):
+        ActionChains(self.driver).move_to_element(element).perform()
+        element.send_keys(Keys.PAGE_DOWN)
 
 
-def safe_name(string):
-    return re.sub('\W', '', re.sub('[.]', '_', string.lower()))
-
-
-def safe_browser_name(d):
-    return '_'.join([safe_name(d[n]) for n in ['os', 'os_version', 'browser', 'browser_version']])
-
-
-def rewrite_for_all_browsers(try_these):
+def rewrite_for_all_browsers(browser_list, times=1):
     """
         Magically make test methods for all desired browsers. Note that this method cannot contain
         the word 'test' or nose will decide it is worth running.
     """
     # TODO: find test methods and rewrite them all
     name = 'test_create_page_and_preview'
-    m = getattr(TestPreview, name)
-    for browser in try_these:
-        new_name = "{name}_{browser}".format(name=name, browser=safe_browser_name(browser))
-        new_function = lambda i, b=browser: m(i, b)
-        new_function.__name__ = new_name
-        setattr(TestPreview, new_name, new_function)
+    test_method = getattr(TestPreview, name)
+    for count in range(1,times+1):
+        for browser in browser_list:
+            new_name = "{name}_{browser}".format(name=name, browser=browser.safe_name())
+            if times > 1:
+                new_name+= "-{}".format(count)
+            new_function = lambda instance, browser_to_use=browser: test_method(instance, browser_to_use)
+            new_function.__name__ = new_name
+            setattr(TestPreview, new_name, new_function)
     delattr(TestPreview, name)
 
 
 if BROWSERS_TO_TRY:
-    rewrite_for_all_browsers(BROWSERS_TO_TRY)
+    rewrite_for_all_browsers(BROWSERS_TO_TRY, TEST_REPETITIONS)
 
 if __name__ == '__main__':
     main()
