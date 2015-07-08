8058dc4267532d300db9574608503f7a57670c53
Norris Hung <norrishung@Norriss-MacBook-Pro.local>
2015-07-07 17:31:22 -0700

    adding styleguide

--- chime/views.py
+++ chime/views.py
@@ -613,6 +613,9 @@ def deploy_key():
     except IOError:
         return Response('Not found.', 404, content_type='text/plain')
 
+@app.route('/styleguide')
+def styleguide():
+    return render_template('styleguide.html')
 
 @app.route('/<path:path>')
 @log_application_errors
