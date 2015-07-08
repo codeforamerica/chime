2123c58635d849885bfc971ee5063cc3118319e0
Thomas <tma@angrymonkey.com>
2015-07-08 16:46:57 -0700

    importing sys and traceback

--- chime/worker.py
+++ chime/worker.py
@@ -5,17 +5,19 @@ Logger = getLogger('chime.worker')
 import os
 import argparse
 import time
+import sys
+import traceback
 
 from .google_api_functions import (
     is_overdue_ga_config, read_ga_config, request_new_google_access_token
-    )
+)
 
 parser = argparse.ArgumentParser(description='Do the things that need doing.')
 
 if __name__ == '__main__':
 
     args = parser.parse_args()
-    
+
     running_state_dir, ga_client_id, ga_client_secret = \
         os.environ['RUNNING_STATE_DIR'], os.environ['GA_CLIENT_ID'], \
         os.environ['GA_CLIENT_SECRET']
@@ -31,7 +33,7 @@ if __name__ == '__main__':
                 token_args = (
                     read_ga_config(running_state_dir).get('refresh_token'),
                     running_state_dir, ga_client_id, ga_client_secret
-                    )
+                )
                 request_new_google_access_token(*token_args)
         except:
             traceback.print_exc(file=sys.stderr)
