e2b349113d534f3647c9037240d4eed7d968e649
Thomas Apodaca <tma@angrymonkey.com>
2015-07-27 11:15:16 -0700

    ignoring task metadata file when reporting conflict to user defaulting to filename when title's not available

--- chime/error_functions.py
+++ chime/error_functions.py
@@ -7,7 +7,7 @@ from urllib import quote
 from urlparse import urlparse
 from os.path import join
 from .view_functions import get_repo, strip_index_file, path_display_type, get_value_from_front_matter
-from .repo_functions import CONFLICT_ACTION_DELETED
+from .repo_functions import CONFLICT_ACTION_DELETED, TASK_METADATA_FILENAME
 
 EMAIL_SUBJECT_TEXT = u'Chime Error Report'
 EMAIL_BODY_PREFIX = u'\n\n----- Please add any relevant details above this line -----\n\n'
@@ -60,17 +60,21 @@ def summarize_conflict_details(error):
     conflict_files = error.files()
     summary = []
     for id_file in conflict_files:
+        # skip the task metadata file
+        if TASK_METADATA_FILENAME in id_file['path']:
+            continue
+
         file_description = {'actions': id_file['actions'].title()}
         edit_path = u''
         display_type = u''
-        title = u''
+        title = id_file['path'].split('/')[-1]
         # construct location info if the file wasn't deleted
         if id_file['actions'] != CONFLICT_ACTION_DELETED:
             file_loc = join(repo.working_dir, id_file['path'])
             dir_path = strip_index_file(id_file['path'])
             dir_loc = join(repo.working_dir, dir_path)
             display_type = path_display_type(dir_loc)
-            title = get_value_from_front_matter('title', file_loc)
+            title = get_value_from_front_matter('title', file_loc) or title
             edit_path = join(u'/tree/{}/edit/'.format(branch_name), dir_path)
 
         file_description['edit_path'] = edit_path
